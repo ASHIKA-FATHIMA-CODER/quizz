29df58bc171f87e4fbf0e59be4c768446595205d
ASHIKA-FATHIMA-CODER <139641396+ASHIKA-FATHIMA-CODER@users.noreply.github.com>
2023-08-19 21:10:47 +0530

    Add files via upload

---
+++ quizz-app-1/questions.js
@@ -0,0 +1,206 @@
+let questions = [
+    {
+        numb: 1,
+        question: " What does the abbreviation HTML stand for?",
+        answer: "A. HyperText Markup Language",
+        options:[
+            "A. HyperText Markup Language",
+            "B. HyperText Machine Language",
+            "C. HyperText Marking Language",
+            "D. HighText Marking Language"
+
+        ]
+
+       
+    },
+    {
+        numb: 2,
+        question: " Who is the father of HTML?",
+        answer: "B. Tim Berners-Lee",
+        options:[ "A. Rasmus Lerdorf",
+        "B. Tim Berners-Lee",
+        "C. Brendan Eich",
+        "D. Sergey Brin"
+        
+
+        ]
+
+       
+    },
+    {
+        numb: 3,
+        question: "Which of the following is used to read an HTML page and render it?",
+        answer: "C. Web browser",
+        options:[ "A.  Web server",
+            "B. Web network",
+            "C. Web browser",
+            "D. Web matrix"
+            
+
+        ]
+
+       
+    },
+    {
+        numb:4,
+        question: "Which element is used for or styling HTML5 layout? ",
+        answer: "A. CSS",
+        options:["A. CSS",
+            "B. jQuery",
+            "C. JavaScript",
+            "D. PHP"
+
+        ]
+
+       
+    },
+    {
+        numb:5,
+        question: "What is DOM in HTML?",
+        answer: "D. Convention for representing and interacting with objects in html documents",
+        options:["A. Language dependent application programming",
+        "B. Hierarchy of objects in ASP.NET",
+        "C. Application programming interface",
+        "D. Convention for representing and interacting with objects in html documents"
+
+        ]
+
+       
+    },
+    {
+        numb:6,
+        question: "What is CSS?",
+        answer: "D. All of the mentioned",
+        options:["A. CSS is a style sheet language",
+        "B. CSS is designed to separate the presentation and content, including layout, colors, and fonts",
+        "C. CSS is the language used to style the HTML documents",
+        "D. All of the mentioned"
+
+           
+        ]
+
+       
+    },
+    {
+        numb:7,
+        question: "Which of the following CSS framework is used to create a responsive design?",
+        answer: "A. bootstrap",
+        options:["A. bootstrap",
+        "B. django",
+        "C. rails",
+        "D. larawell"
+
+        ]
+
+       
+    }, 
+    
+    {
+        numb:8,
+        question: "Which of the following CSS style property is used to specify an italic text?",
+        answer: "C. font-style",
+        options:["A. style",
+        "B. font",
+        "C. font-style",
+        "D. @font-face"
+
+        ]
+
+       
+    },
+    {
+        numb:9,
+        question: "Which of the following CSS property is used to make the text bold?",
+        answer: "B. font-weight: bold",
+        options:["A. text-decoration: bold",
+        "B. font-weight: bold",
+        "C. font-style: bold",
+        "D. text-align: bold"
+
+        ]
+
+       
+    },
+
+    {
+        numb:10,
+        question: "Are the negative values allowed in padding property?",
+        answer: "B. No",
+        options:["A. Yes",
+        "B. No",
+        "C. Can't say",
+        "D. May be"
+
+        ]
+
+       
+    },
+    {
+        numb:11,
+        question: "Which type of JavaScript language is ___",
+        answer: "C. Object-Based",
+        options:["A. Object-Oriented",
+        "B. Assembly-language",
+        "C. Object-Based",
+        "D. High-level"
+
+        ]
+
+       
+    },
+    {
+        numb:12,
+        question: "The function and  var are known ",
+        answer: "C. Declaration statements",
+        options:["A. Keywords",
+        "B. Data types",
+        "C. Declaration statements",
+        "D. Prototypes"
+
+        ]
+
+       
+    },
+    {
+        numb:13,
+        question: "Arrays in JavaScript are defined by which of the following statements?",
+        answer: "A. It is an ordered list of values",
+        options:["A. It is an ordered list of values",
+        "B. It is an ordered list of objects",
+        "C. It is an ordered list of string",
+        "D. It is an ordered list of functions"
+
+        ]
+
+       
+    },
+    {
+        numb:14,
+        question: "Which one of the following is the correct way for calling the JavaScript code? ",
+        answer: "D. Function/Method",
+        options:["A. Preprocessor",
+        "B. Triggering Event",
+        "C. RMI",
+        "D. Function/Method"
+    ]
+},
+    
+
+        
+     
+    {
+        numb:15,
+        question: "Which of the following number object function returns the value of the number?",
+        answer: "D. valueOf()",
+        options:["A. toString()",
+        "B. toLocaleString()",
+        "C. toPrecision()",
+        "D. valueOf()"
+        ]
+
+       
+    },
+   
+   
+   
+];
